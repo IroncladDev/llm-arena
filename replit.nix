604ef95255b37fd61d8702b7167cf37956b9d445
IroncladDev <connerow1115@gmail.com>
2024-02-17 17:21:38 +0000

    replit files

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+      pkgs.postgresql
+      pkgs.bun
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-18_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
